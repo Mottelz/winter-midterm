292cd9441f223a898feeabd81f1dffa48abca51f
Mottel <shachna@gmail.com>
2024-03-14 13:06:58 -0400

    initial commit

---
+++ tests/routers/comic.spec.js
@@ -0,0 +1,76 @@
+const request = require("supertest");
+const app = require("../../src/app");
+
+describe("Patient home route", () => {
+    test("should responed to GET", async () => {
+        const response = await request(app).get('/patient/home');
+        expect(response.statusCode).toBe(200);
+        expect(response.body.route).toBe('/patient/home');
+    });
+});
+
+describe("All comics route", () => {
+    test("Should return all comics", async () => {
+        const response = await request(app).get('/comic/all');
+        expect(response.statusCode).toBe(200);
+        expect(response.body).toMatch({
+            comics: [
+                {
+                    series: 'Superman',
+                    issue: 1,
+                },
+                {
+                    series: 'Superman',
+                    issue: 2,
+                },
+                {
+                    series: 'Superman',
+                    issue: 3,
+                },
+                {
+                    series: 'Batman',
+                    issue: 1,
+                },
+                {
+                    series: 'Batman',
+                    issue: 2,
+                }
+            ],
+            count: 5
+        })
+    })
+});
+
+describe("Series route", () => {
+    test("Should return based on series", async () => {
+        const response = await request(app).get('/comic/series/batman');
+        expect(response.statusCode).toBe(200);
+        expect(response.body).toMatch({
+            comics: [
+                {
+                    series: 'Batman',
+                    issue: 1,
+                },
+                {
+                    series: 'Batman',
+                    issue: 2,
+                }
+            ],
+            count: 2
+        })
+    })
+});
+
+describe("Single issue route", () => {
+    test("Should return based on issue", async () => {
+        const response = await request(app).get('/comic/series/batman/issue/1');
+        expect(response.statusCode).toBe(200);
+        expect(response.body).toMatch({
+            issue:
+                {
+                    series: 'Batman',
+                    issue: 1,
+                },
+        })
+    })
+});
